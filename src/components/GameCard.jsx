fbf36f6239ff452ca03f6cd71d113745789564e7
Kyle Walker <kylewalker@Caths-MacBook-Air.local>
2019-05-29 14:42:51 +0100

    testing Card styling

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from '@reach/router';
+import { Card } from '@material-ui/core';
 
 const GameCard = (props) => {
   const { game } = props;
   return (
     <div>
-      <Link to={`/play/${game.title}`}>
-        <h1>{game.title}</h1>
-        <p>{game.description}</p>
-        <p>levels: {game.levels.length()}</p>
-      </Link>
+      <Card>
+        <Link to={`/play/${game.title}`}>
+          <h1>{game.title}</h1>
+          <p>{game.description}</p>
+          <p>levels: {game.levels.length()}</p>
+        </Link>
+      </Card>
     </div>
   );
 };
