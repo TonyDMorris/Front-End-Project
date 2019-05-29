fcd3d1522ba66066befb49eeba9246059dc7d85d
Rob Clegg <roberto198@github.com>
2019-05-29 16:25:45 +0100

    changed game card

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,17 +1,6 @@
 import React from 'react';
 import { Link } from '@reach/router';
 
-const GameCard = props => {
-	const { game } = props;
-	return (
-		<div>
-			<Link to={`/play/${game.title}`}>
-				<h1>{game.title}</h1>
-				<p>{game.description}</p>
-				<p>levels: {game.levels.length}</p>
-			</Link>
-		</div>
-	);
-};
+const GameCard = props => {};
 
 export default GameCard;
