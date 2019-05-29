edd65e3694fd567f0ba1ea8ad28489d3a083b4e2
Natalalia <n.olmo.megias@gmail.com>
2019-05-29 15:06:32 +0100

    header setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 function Header() {
-  return <h1>Header here!</h1>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            arial-label="Menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            App Name
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
 export default Header;
