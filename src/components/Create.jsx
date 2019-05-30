e1e53d4941b63e0a86ba3e1d31845708ea2125be
TonyDMorris <anthonymorris86@googlemail.com>
2019-05-30 10:24:38 +0100

    working on create component

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Card, Typography, TextField } from "@material-ui/core";
 
-function Create() {
-  return <h1>Create here!</h1>;
+class Create extends React.Component {
+  state = { title: "", description: "", completion: "", levels: [] };
+
+  render() {
+    return (
+      <Card>
+        <Typography>flavour text</Typography>
+        <TextField
+          label="Title:"
+          onChange={e => this.handleChange("title", e.target.value)}
+        />
+        <TextField
+          label="Description:"
+          onChange={e => this.handleChange("description", e.target.value)}
+        />
+        <TextField
+          label="Completion text:"
+          onChange={e => this.handleChange("completion", e.target.value)}
+        />
+      </Card>
+    );
+    // title
+
+    // description
+
+    //Form
+
+    //win message
+    //submit button
+  }
+  handleChange = (str, value) => {
+    this.setState({ [str]: value });
+  };
+  //handleLevel = ()=>{
+  //handleSubmit=()=>{
+  //navigate to created game
 }
 
 export default Create;
