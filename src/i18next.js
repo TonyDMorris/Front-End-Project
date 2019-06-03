be3b2b4e2268965f49cf05f33e4a877b21c0e871
Natalalia <n.olmo.megias@gmail.com>
2019-06-03 10:21:56 +0100

    create level component translated

--- src/i18next.js
+++ src/i18next.js
@@ -18,7 +18,17 @@ const resources = {
       "Game completion message": "Game completion message:",
       levels: "Levels:",
       "introduction paragraph":
-        "Welcome to TheHunt, where you can challenge your friends to hunt pretty much whatever you want! Set some clues(as hard or as easy as you like), find some objects, beat your friends! Can you find everything without using any clues??Pick one of the games, or create your own!"
+        "Welcome to TheHunt, where you can challenge your friends to hunt pretty much whatever you want! Set some clues(as hard or as easy as you like), find some objects, beat your friends! Can you find everything without using any clues??Pick one of the games, or create your own!",
+      "Create new level": "Create level:",
+      "Main clue": "Main clue:",
+      "Second clue": "Second clue:",
+      "Third clue": "Third clue:",
+      "Select win condition": "Select a win condition:",
+      Text: "Text:",
+      Image: "Image",
+      GPS: "GPS",
+      "Level completion message": "Level completion message:",
+      "Add Level": "Add Level"
     }
   },
   es: {
@@ -35,7 +45,17 @@ const resources = {
       "Game completion message": "Mensaje final de juego:",
       levels: "Niveles:",
       "introduction paragraph":
-        "Bienvenido a TheHunt, ¡dónde tú puedes retar a tus amigos a lo que sea! Pon algunas pistas (tan difíciles o fáciles como tú quieras), encuentra objetos, ¡machácales! ¿Eres capaz de encontrar todo sin necesitar ninguna pista? ¡Elige uno de los juegos, o crea el tuyo propio!"
+        "Bienvenido a TheHunt, ¡dónde tú puedes retar a tus amigos a lo que sea! Pon algunas pistas (tan difíciles o fáciles como tú quieras), encuentra objetos, ¡machácales! ¿Eres capaz de encontrar todo sin necesitar ninguna pista? ¡Elige uno de los juegos, o crea el tuyo propio!",
+      "Create new level": "Crear Nivel:",
+      "Main clue": "Primera pista:",
+      "Second clue": "Segunda pista:",
+      "Third clue": "Tercera pista:",
+      "Select win condition": "Elige como tiene que ser la respuesta:",
+      Text: "Texto:",
+      Image: "Imagen",
+      GPS: "GPS",
+      "Level completion message": "Mensaje al pasar de nivel:",
+      "Add Level": "Añadir Nivel"
     }
   }
 };
