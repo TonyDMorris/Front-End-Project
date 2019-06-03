6736cbc8356de68b0e0a2082745c0fc0a347253a
Rob Clegg <roberto198@github.com>
2019-06-03 14:46:14 +0100

    merge commit

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
 import Axios from "axios";
 import { Typography, TextField, Button } from "@material-ui/core";
+import { withStyles } from "@material-ui/styles";
+import PropTypes from "prop-types";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const styles = {
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    width: "90%"
+    // marginTop: theme.spacing(3)
+  },
+  table: {
+    maxWidth: 650
+  }
+};
 
 class LeaderBoard extends Component {
   state = {
-    leaderBoard: null
+    leaderBoard: []
   };
 
   componentDidMount() {
@@ -20,13 +40,14 @@ class LeaderBoard extends Component {
       .catch(err => console.log(err));
   }
   render() {
+    const { classes } = this.props;
     return (
       <div>
         <Typography variant='h4'>Welcome to the leaderboard!</Typography>
         <Typography variant='body1'>
           Please enter your name to add your score to the leaderboard!
         </Typography>
-        <form onSubmit={this.submitScore}>
+        <form onSubmit={this.submitScore} className={classes.form}>
           <TextField
             id='standard-name'
             label='Name'
@@ -48,6 +69,32 @@ class LeaderBoard extends Component {
                 </div>
               );
             })}
+
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Username</TableCell>
+                <TableCell align='right'>Score</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.leaderBoard &&
+                this.state.leaderBoard
+                  .sort((a, b) => {
+                    return b.score - a.score;
+                  })
+                  .map(score => (
+                    <TableRow key={score.username}>
+                      <TableCell component='th' scope='row'>
+                        {score.username}
+                      </TableCell>
+                      <TableCell align='right'>{score.score}</TableCell>
+                    </TableRow>
+                  ))}
+            </TableBody>
+          </Table>
+        </Paper>
       </div>
     );
   }
@@ -59,6 +106,11 @@ class LeaderBoard extends Component {
   submitScore = e => {
     console.log("submitSccore");
     e.preventDefault();
+    Axios.patch("https://mongo-flask-api.herokuapp.com/leaderboards", {
+      game_id: this.props.game_id,
+      username: this.state.input,
+      score: this.props.score
+    });
     this.setState(prevState => {
       const newBoard = [
         { username: this.state.input, score: this.props.score },
@@ -66,12 +118,11 @@ class LeaderBoard extends Component {
       ];
       return { leaderBoard: newBoard };
     });
-    Axios.patch("https://mongo-flask-api.herokuapp.com/leaderboards", {
-      game_id: this.props.game_id,
-      username: this.state.input,
-      score: this.props.score
-    });
   };
 }
 
-export default LeaderBoard;
+LeaderBoard.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(LeaderBoard);
