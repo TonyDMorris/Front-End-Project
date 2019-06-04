44fd7aadd4c9a698c8fe33d8587124de2539c9a1
Natalalia <n.olmo.megias@gmail.com>
2019-06-04 12:13:27 +0100

    cards

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,32 +1,54 @@
 import React from "react";
 import { Link as ReachLink } from "@reach/router";
-
-import { Card, Link, Typography } from "@material-ui/core";
-
+import { Card, Link, Typography, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { useTranslation } from "react-i18next";
+import theme from "../theme";
+
+const useStyles = makeStyles({
+  card: {
+    width: 500,
+    height: 175,
+    ["@media (min-width:300px)"]: {
+      width: 300
+    },
+    padding: 12,
+    bgcolor: "background.paper",
+    borderColor: "text.primary",
+    borderRadius: 16,
+    border: "100 px",
+    margin: theme.spacing(1)
+  },
+  title: {
+    marginBottom: 10
+  },
+  pos: {
+    marginTop: 10
+  }
+});
 
 const GameLink = React.forwardRef((props, ref) => (
   <ReachLink innerRef={ref} {...props} />
 ));
 
 const GameCard = props => {
+  const classes = useStyles();
+
   const { t } = useTranslation();
 
   const { game } = props;
-  console.log(game);
   return (
-    <div key={game.game_id}>
-      <Card>
-        <Link component={GameLink} to={`/play/${game.id}`}>
-          <Typography variant="h3">{game.title}</Typography>
-          <Typography variant="body1"> {game.description}</Typography>
-          <Typography>
-            {" "}
-            {t("levels")} {game.levels.length}
-          </Typography>
-        </Link>
-      </Card>
-    </div>
+    <Button variant="outlined" className={classes.card} key={game.game_id}>
+      <Link component={GameLink} to={`/play/${game.id}`}>
+        <Typography variant="h5" className={classes.title}>
+          {game.title}
+        </Typography>
+        <Typography variant="body1"> {game.description}</Typography>
+        <Typography variant="body2" className={classes.pos}>
+          {t("levels")} {game.levels.length}
+        </Typography>
+      </Link>
+    </Button>
   );
 };
 
