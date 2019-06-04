4d489798378d512ae5c5c860f295696b320f2474
Kyle Walker <kylewalker@Caths-MacBook-Air.local>
2019-06-04 09:44:05 +0100

    re-adding theme.js file

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+import { createMuiTheme } from "@material-ui/core";
+import blue from "@material-ui/core/colors/blue";
+import green from "@material-ui/core/colors/green";
+// import PirateFont from "./PirateFont.tff";
+
+export default createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: green
+  },
+  typography: {
+    fontFamily: "'Montserrat', sans-serif",
+    fontSize: 16,
+    fontWeightLight: 500,
+    fontWeightRegular: 600,
+    fontWeightMedium: 700
+  }
+});
