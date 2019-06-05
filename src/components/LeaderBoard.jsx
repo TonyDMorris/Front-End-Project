8037e87b0f665a01c348f33a23d091058578cbd2
Rob Clegg <roberto198@github.com>
2019-06-05 12:00:40 +0100

    resized homescreen font

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -14,14 +14,25 @@ import {
 import { withStyles } from "@material-ui/styles";
 import PropTypes from "prop-types";
 import { getLeaderBoard, submitScore } from "../Api/Api";
+
 const styles = {
   root: {
     display: "flex",
     justifyContent: "center",
-    width: "auto"
+    width: "auto",
+    backgroundImage: `url(${chest})`,
+    backgroundSize: "cover"
   },
   table: {
     maxWidth: 650
+  },
+  form: {
+    display: "flex",
+    flexDirection: "row",
+    alignSelf: "center"
+  },
+  table: {
+    backgroundColor: "rgba(255,255,255,0.75)"
   }
 };
 
@@ -40,34 +51,40 @@ class LeaderBoard extends Component {
     const { classes } = this.props;
     return (
       <div>
-        <Typography variant="h4">Welcome to the leaderboard!</Typography>
-        <Typography variant="body1">
+        <Typography variant='h4'>Welcome to the leaderboard!</Typography>
+        <Typography variant='body1'>
           Please enter your name to add your score to the leaderboard!
         </Typography>
-        <form onSubmit={this.submitScore} className={classes.form}>
-          <TextField
-            id="standard-name"
-            label="Name"
-            margin="normal"
-            onChange={(e) => {
-              this.handleInput(e.target.value);
-            }}
-          />
-          <Button onClick={this.submitScore}>Submit Score!</Button>
+
+        <form onSubmit={this.submitScore}>
+          <div className={classes.form}>
+            <TextField
+              id='standard-name'
+              label='Name'
+              margin='normal'
+              onChange={e => {
+                this.handleInput(e.target.value);
+              }}
+            />
+            <Button
+              style={{
+                marginTop: "auto"
+              }}
+              onClick={this.submitScore}
+              variant='outlined'
+              color='inherit'
+            >
+              Submit Score!
+            </Button>
+          </div>
         </form>
 
-        <Paper
-          className={classes.root}
-          style={{ backgroundImage: `url(${chest})`, backgroundSize: "cover" }}
-        >
-          <Table
-            style={{ backgroundColor: "rgba(255,255,255,0.75)" }}
-            className={classes.table}
-          >
+        <Paper className={classes.root} style={{}}>
+          <Table style={{}} className={classes.table}>
             <TableHead>
               <TableRow>
                 <TableCell>Username</TableCell>
-                <TableCell align="right">Score</TableCell>
+                <TableCell align='right'>Score</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -76,12 +93,12 @@ class LeaderBoard extends Component {
                   .sort((a, b) => {
                     return b.score - a.score;
                   })
-                  .map((score) => (
+                  .map(score => (
                     <TableRow key={score.username}>
-                      <TableCell component="th" scope="row">
+                      <TableCell component='th' scope='row'>
                         {score.username}
                       </TableCell>
-                      <TableCell align="right">{score.score}</TableCell>
+                      <TableCell align='right'>{score.score}</TableCell>
                     </TableRow>
                   ))}
             </TableBody>
@@ -91,17 +108,17 @@ class LeaderBoard extends Component {
     );
   }
 
-  handleInput = (username) => {
+  handleInput = username => {
     this.setState({ username });
   };
 
-  submitScore = (e) => {
+  submitScore = e => {
     const { username } = this.state;
     const { score, game_id } = this.props;
     const highScore = { game_id, username, score };
     e.preventDefault();
     submitScore(highScore);
-    this.setState((prevState) => {
+    this.setState(prevState => {
       console.log(prevState);
       const leaderBoard = [highScore, ...prevState.leaderBoard];
       return { leaderBoard };
