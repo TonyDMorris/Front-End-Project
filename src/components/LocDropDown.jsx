f0614a8f16b246702312c9495d9e3dcb01b174a1
Natalalia <n.olmo.megias@gmail.com>
2019-06-05 10:10:28 +0100

    language button style done

--- src/components/LocDropDown.jsx
+++ src/components/LocDropDown.jsx
@@ -1,17 +1,33 @@
 import React, { useState } from "react";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
-import { Select, InputLabel } from "@material-ui/core";
+import { Select, InputLabel, makeStyles } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
 
+const useStyles = makeStyles({
+  form: {
+    width: 100,
+    height: 25
+  },
+  input: {
+    "&:after": {
+      borderBottomColor: "black"
+    }
+  }
+});
+
 const LocDropDown = () => {
+  const classes = useStyles();
   const { t, i18n } = useTranslation();
   const [lang, setLang] = useState("English");
   const langObj = { en: "English", es: "Spanish" };
   return (
-    <FormControl>
-      <InputLabel>{t("current-language")}</InputLabel>
+    <FormControl className={classes.form}>
+      <InputLabel style={{ fontSize: "25px" }} focused={false}>
+        {t("current-language")}
+      </InputLabel>
       <Select
+        className={classes.input}
         value={"lang"}
         onChange={e => {
           i18n.changeLanguage(e.target.value);
